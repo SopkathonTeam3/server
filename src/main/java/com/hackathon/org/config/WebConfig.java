f7417bcf63014d4ba33c4ffa4951c740837ffd85
seunghaLim <tmdgk490255@gmail.com>
2023-05-21 06:15:16 +0900

    [FIX] cors 이슈 수정

--- src/main/java/com/hackathon/org/config/WebConfig.java
+++ src/main/java/com/hackathon/org/config/WebConfig.java
@@ -19,9 +19,6 @@ public class WebConfig implements WebMvcConfigurer {
                 // 모든 HTTP Method를 허용한다.
                 .allowedMethods("*")
                 // HTTP 요청의 Header에 어떤 값이든 들어갈 수 있도록 허용한다.
-                .allowedHeaders("*")
-                // 자격증명 사용을 허용한다.
-                // 해당 옵션 사용시 allowedOrigins를 * (전체)로 설정할 수 없다.
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
