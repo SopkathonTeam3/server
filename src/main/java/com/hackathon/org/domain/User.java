2b4be7f3e09550212cdb9562236ff7b1751d3d89
Jin409 <sualng123@gmail.com>
2023-05-21 01:16:53 +0900

    [FEAT] user 엔티티 생성 #2

---
+++ src/main/java/com/hackathon/org/domain/User.java
@@ -0,0 +1,32 @@
+package com.hackathon.org.domain;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "background_image_ID")
+    private BackgroundImage backgroundImage;
+
+    private String name;
+}
