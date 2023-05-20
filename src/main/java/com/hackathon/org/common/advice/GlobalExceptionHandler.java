c0025355bf9bb5ee3be9eae77f1ddb379975cf9d
Jin409 <sualng123@gmail.com>
2023-05-21 05:36:16 +0900

    [FIX] 에러 핸들링 기능 수정 #5

--- src/main/java/com/hackathon/org/common/advice/GlobalExceptionHandler.java
+++ src/main/java/com/hackathon/org/common/advice/GlobalExceptionHandler.java
@@ -10,11 +10,11 @@ import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     /*
